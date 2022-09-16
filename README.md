fb4807272bab99baedea8e8212f31ad901787516
Soosi18 <sarvagya.pandey2000@yahoo.com>
2022-09-16 02:01:58 -0700

    Add feature to check for repeating characters in an array. Add tests to ensure the feature works as intended

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 499-Individual-Exercise
+
+##### Feature 2 - Checking for repeating/duplicate letters in a char array
+CheckRepeating.java declares and initializes a char array of size 5, with random letters from a-z. The contents of this random array are then printed
+to console. The method checkRepeatingLetter() sorts this char array alphabetically and checks for duplicate letters by comparing the letter at each 
+index to the next. If there is a repeating letter, it returns true, otherwise it returns false. The results of this check are then printed to console 
+with the help of the conditional operator. CheckRepeatingTest.java contains tests to make sure this method is running correctly.
---
+++ src/CheckRepeating.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class CheckRepeating {
+
+	public static void main(String[] args) {
+		
+		//declare and initialize 'letters' array with random values
+		char[] letters = new char[5];
+		Random r = new Random();
+		
+		for(int i = 0; i < letters.length; i++) {
+			letters[i] = (char) (r.nextInt(26) + 'a');
+		}
+		
+		//print contents of letters array
+		System.out.println("letters Array: " + Arrays.toString(letters) + "\n\n");
+		
+		//print results of checkRepeatingLetter() method
+		System.out.println("Is there a repeating alphabet in the letters Array? : " + (checkRepeatingLetter(letters) ? "Yes" : "No"));
+	}
+	
+	
+	//check if any letter repeats in 'letters' array. If yes, returns true, otherwise returns false.
+	public static boolean checkRepeatingLetter(char[] letters) {
+		Arrays.sort(letters);
+		for(int i = 0; i < letters.length - 1; i++) {
+			if(letters[i] == letters[i+1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
